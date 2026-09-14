62d81886d99a8b913eea1a3af495617650ca45e9
SokcheatSrorng <sokcheatsrorng@gmail.com>
2026-09-14 19:28:04 +0700

    Factor: Deploy version2

--- src/app/products/[...id]/page.tsx
+++ src/app/products/[...id]/page.tsx
@@ -1,6 +1,39 @@
 
 import ProductDetailListComponent from "@/components/products/ProductDetailListComponent";
 
+import type { Metadata, ResolvingMetadata } from 'next'
+ 
+type Props = {
+  params: Promise<{ id: number }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+ 
+// dynamic metadata & opengraph 
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const { id } = await params;
+ 
+  // fetch data
+  const product = await fetch(`https://fakestoreapi.com/products/${id}`)
+  .then((res) => res.json())
+ 
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || []
+ 
+  return {
+    title: product.title,
+    description: product.description,
+    openGraph: {
+      title: product.title,
+      description: product.description,
+      images: [`${product.image}`, ...previousImages],
+    },
+  }
+}
+ 
 export default async function DetailProductPage(
   {
   params
